788753ab5c693ec2357b8c29c5f6100e576fd375
Ann Allan <ann.cristina.allan@gmail.com>
2018-04-08 17:10:49 -0400

    Create comments part 2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
+var Comment = require("./models/comment");
 var seedDB = require("./seeds");
 
 
@@ -74,7 +75,32 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
       res.render("comments/new", {campground: campground});
     }
   })
-})
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+  // lookup campground using ID
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      // create new comment
+      Comment.create(req.body.comment, function(err, comment){
+        if(err) {
+          console.log(err)
+        } else {
+          // connect new comment to campground
+          campground.comments.push(comment);
+          campground.save();
+          // redirect campground show page
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      })
+
+
+    }
+  })
+});
 
 app.listen(app.get('port'), function(){
   console.log("The YelpCamp Server has started");
