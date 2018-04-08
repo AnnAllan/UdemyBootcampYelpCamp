d18a2ab76d56f27598bc348793d4981bc622d6a5
Ann Allan <ann.cristina.allan@gmail.com>
2018-04-08 17:56:47 -0400

    Add styling comment part 2

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var seedDB = require("./seeds");
 mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 
 app.set('port', process.env.PORT || 3000)
 app.set("view engine", "ejs");
