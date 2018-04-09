e97824438671e3c4b8fc37c45ea773f73eb89354
Ann Allan <ann.cristina.allan@gmail.com>
2018-04-09 17:25:18 -0400

    Add campground destroy

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -79,6 +79,17 @@ router.put("/:id", function(req, res){
   // redirect to show page
 })
 
+// DESTROY campground route
+router.delete("/:id", function(req, res){
+  Campground.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated(0)){
     return next();
