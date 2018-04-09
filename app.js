aa8807b8d6f2c1a342d22c982026e947b5a054c9
Ann Allan <ann.cristina.allan@gmail.com>
2018-04-08 20:38:12 -0400

    Add auth part 3

--- app.js
+++ app.js
@@ -137,6 +137,17 @@ app.post("/register", function(req, res){
   });
 });
 
+// show login form
+app.get("/login", function(req, res){
+  res.render("login");
+});
+// handle login logic
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }), function(req, res){
+});
 
 app.listen(app.get('port'), function(){
   console.log("The YelpCamp Server has started");
