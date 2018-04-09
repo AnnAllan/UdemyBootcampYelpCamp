c3367b145976aa13c3362d689dbd7a13d67f1655
Ann Allan <ann.cristina.allan@gmail.com>
2018-04-08 21:24:22 -0400

    Add auth part 4

--- app.js
+++ app.js
@@ -80,9 +80,9 @@ Campground.findById(req.params.id).populate("comments").exec(function(err, found
 });
 });
 
+//cOMMENTS
 
-
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, campground){
     if(err){
       console.log(err);
@@ -92,7 +92,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
   })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
   // lookup campground using ID
   Campground.findById(req.params.id, function(err, campground){
     if(err){
@@ -148,6 +148,19 @@ app.post("/login", passport.authenticate("local",
     failureRedirect: "/login"
   }), function(req, res){
 });
+
+// logout route
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated(0)){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(app.get('port'), function(){
   console.log("The YelpCamp Server has started");
