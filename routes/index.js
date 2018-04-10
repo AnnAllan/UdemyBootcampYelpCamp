7749eb7089a667652013591278642f97042dbfac
Ann Allan <ann.cristina.allan@gmail.com>
2018-04-09 20:09:08 -0400

    Refactor middleware

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,57 @@
 var express = require("express");
-var router = express.Router();
+var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
-// ROOT route
+//root route
 router.get("/", function(req, res){
-  res.render("landing");
+    res.render("landing");
 });
 
 // show register form
 router.get("/register", function(req, res){
-  res.render("register");
+   res.render("register");
 });
 
-// handle sign up logic
+//handle sign up logic
 router.post("/register", function(req, res){
-  var newUser = new User({username: req.body.username});
-  User.register(newUser, req.body.password, function(err, user){
-    if(err){
-      console.log(err);
-      return res.render("register")
-    }
-    passport.authenticate("local")(req, res, function(){
-      res.redirect("/campgrounds");
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/campgrounds");
+        });
     });
-  });
 });
 
-// show login form
+//show login form
 router.get("/login", function(req, res){
-  res.render("login");
+   res.render("login");
 });
 
-// handle login logic
+//handling login logic
 router.post("/login", passport.authenticate("local",
-  {
-    successRedirect: "/campgrounds",
-    failureRedirect: "/login"
-  }), function(req, res){});
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
 
 // logout route
 router.get("/logout", function(req, res){
-  req.logout();
-  res.redirect("/campgrounds");
+   req.logout();
+   res.redirect("/campgrounds");
 });
 
-// middleware
+//middleware
 function isLoggedIn(req, res, next){
-  if(req.isAuthenticated(0)){
-    return next();
-  }
-  res.redirect("/login");
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
 }
 
 module.exports = router;
